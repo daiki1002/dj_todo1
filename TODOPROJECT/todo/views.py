4bd606761671a2c4b8d3b0ce6b858834e1096992
Kambayashi Daiki <ile.imeh.s37@gmail.com>
2021-01-14 11:35:15 +0900

    finish local

--- TODOPROJECT/todo/views.py
+++ TODOPROJECT/todo/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db import IntegrityError
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from .models import TodoModel
+from django.urls import reverse_lazy
 
-def todo(request):
-	return HttpResponse('Hello!')
+
+
+class TodoList(ListView):
+    template_name = 'index.html'
+    model = TodoModel
+
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
+    model = TodoModel
+
+class TodoDelete(DeleteView):
+    template_name = 'delete.html'
+    model = TodoModel
+    success_url = reverse_lazy('list')
+
+class TodoUpdate(UpdateView):
+    template_name = 'update.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'duedate')
+    success_url = reverse_lazy('list')
